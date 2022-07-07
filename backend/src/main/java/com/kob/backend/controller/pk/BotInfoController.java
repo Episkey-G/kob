a3fc94b5842679b040c8456391deaa89015ec243
Episkey-G <1986053226mo@gmail.com>
2022-07-07 23:42:54 +0800

    项目骨架搭建

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -13,16 +11,10 @@ import java.util.Map;
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public List<Map<String, String>> getBotInfo() {
-        List<Map<String, String>> list = new LinkedList<>();
+    public Map<String, String> getBotInfo() {
         Map<String, String> bot1 = new HashMap<>();
         bot1.put("name", "tiger");
         bot1.put("rating", "1500");
-        Map<String, String> bot2 = new HashMap<>();
-        bot2.put("name", "apple");
-        bot2.put("rating", "1800");
-        list.add(bot1);
-        list.add(bot2);
-        return list;
+        return bot1;
     }
 }
